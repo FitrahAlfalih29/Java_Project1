80a6f6b60ce735a1de90f4d605d006f065909497
FitrahAlfalih29 <55485326+FitrahAlfalih29@users.noreply.github.com>
2019-10-03 13:35:05 +0700

    first commit

---
+++ src/project1/app1.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class app1 {
+
+	/**
+	 * 
+	 */
+	public static void main(String[] args){ 
+		//TODO Ao
+		System.out.print(new variabel().alamat);
+	}
+}
---
+++ src/project1/variabel.java
@@ -0,0 +1,7 @@
+package project1;
+
+public class variabel {
+	public String nama="FitrahAlfalih";
+	public String alamat="jlnsunangiri";
+	public String usia="16";
+}
